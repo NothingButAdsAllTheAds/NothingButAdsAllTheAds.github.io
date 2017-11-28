d415bae776eccfc07d37d891d5e2dfa8e585212b
Aaron Stanek <nope>
2017-11-28 14:10:21 -0800

    bugfix

--- js/content.js
+++ js/content.js
@@ -10,6 +10,8 @@ function generate_content()
       q = document.createElement("td");
       q.innerHTML = "<iframe src=\"//rcm-na.amazon-adsystem.com/e/cm?o=1&p=9&l=ez&f=ifr&linkID=7eb1b96497a564498a8a4b1cf96736d6&t=inthelink-20&tracking_id=inthelink-20\" width=\"180\" height=\"150\" scrolling=\"no\" border=\"0\" marginwidth=\"0\" style=\"border:none;\" frameborder=\"0\"></iframe>";
       b.appendChild(q);
+      q = document.createTextNode(" ");
+      b.appendChild(q);
     }
   }
 }
