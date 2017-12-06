7fb186773b3d26e442fb8ba2dc4c18a24d13ba55
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2017-12-05 19:19:48 -0800

    Made Economics fixed.

--- js/economics.js
+++ js/economics.js
@@ -1,11 +1,15 @@
 function allTheMoney() {
+	var body = document.body;
+	var html = document.documentElement;
+	var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+	var docWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
 	var p = document.createElement("p");
 	var curr = ["$", "£", "€", "¥", "₩"];
 	var t = document.createTextNode(curr[Math.floor(Math.random() * curr.length)]);
 	p.appendChild(t);
 	document.getElementsByTagName("body")[0].appendChild(p);
 	p.classList.add('allTheMoney');
-	p.style.left = (Math.random() * parseInt(window.innerWidth)).toString() + "px";
-	p.style.top = (Math.random() * parseInt(window.innerHeight)).toString() + "px";
-	window.setTimeout(all_the_money, 5);
+	p.style.left = (Math.random() * parseInt(docWidth)).toString() + "px";
+	p.style.top = (Math.random() * parseInt(docHeight)).toString() + "px";
+	window.setTimeout(allTheMoney, 5);
 }
