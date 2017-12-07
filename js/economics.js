912cb80a77c4ea520f3799d6e5789ea17ef4c62f
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2017-12-07 10:52:22 -0800

    Did stuffs

--- js/economics.js
+++ js/economics.js
@@ -4,10 +4,11 @@ function allTheMoney() {
 	var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 	var docWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
 	var p = document.createElement("p");
-	var curr = ["$", "£", "€", "¥", "₩"];
+	var curr = ["$", "£", "€", "¥", "₩", "¢"];
 	var t = document.createTextNode(curr[Math.floor(Math.random() * curr.length)]);
 	p.appendChild(t);
-	document.getElementsByTagName("body")[0].appendChild(p);
+	var body = document.getElementsByTagName("body")[0];
+	body.insertBefore(p, body.firstChild);
 	p.classList.add('allTheMoney');
 	p.style.left = (Math.random() * parseInt(docWidth)).toString() + "px";
 	p.style.top = (Math.random() * parseInt(docHeight)).toString() + "px";
